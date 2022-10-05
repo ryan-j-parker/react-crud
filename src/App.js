db6695deb58971f231c142f7b0f5fca5b1ee68f9
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-10-05 13:23:55 -0700

    add switch to auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
+import Auth from './Auth/Auth';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+      </Switch>
     </div>
   );
 }
