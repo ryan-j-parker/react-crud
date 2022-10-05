5ad26995f9c670cb6f595981eb8f5c3a99e48a03
Lawson <lawsonmcphetridge2020@gmail.com>
2022-10-05 14:40:56 -0700

    got sign up and sign in links to redirect

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Auth.css';
 
 function Auth() {
   return <div className='main-auth'>
 
+
+
     <div className='container-input'>
+      <div className='links-container'>
+        <NavLink to="/auth/sign-up"><p>Sign Up</p></NavLink>
+        <NavLink to="/auth/sign-in"><p>Sign In</p></NavLink>
+      </div>
       <label>Email</label>
-      <input></input>
+      <input className='email-input'></input>
       <label>Password</label>
-      <input></input>
+      <input className='password-input'></input>
       <button>Submit</button>
     </div>
 
