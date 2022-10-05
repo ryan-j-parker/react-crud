34939830296fe7bc94c726d957ac3b9dc8b6c5f5
Lawson <lawsonmcphetridge2020@gmail.com>
2022-10-05 14:29:32 -0700

    got email and password feilds and auth route set up

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './Auth.css';
 
 function Auth() {
-  return <div>Auth</div>;
+  return <div className='main-auth'>
+
+    <div className='container-input'>
+      <label>Email</label>
+      <input></input>
+      <label>Password</label>
+      <input></input>
+      <button>Submit</button>
+    </div>
+
+
+  </div>;
 }
 
 export default Auth;
