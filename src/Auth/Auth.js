a83306739bb9957f688db0f94ffdc854397eb4cb
Lawson <lawsonmcphetridge2020@gmail.com>
2022-10-05 14:55:37 -0700

    added redirect if user and sign in and sign out function

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, Redirect, useParams } from 'react-router-dom';
+import { authUser } from '../services/auth';
 import './Auth.css';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 function Auth() {
-  return <div className='main-auth'>
+  const { type } = useParams();
+  const { user, setUser } = useContext(UserContext);
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  
+  if (user) {
+    <Redirect to="/posts" />;
+  }
+
+  return (<div className='main-auth'>
 
 
 
@@ -12,15 +24,28 @@ function Auth() {
         <NavLink to="/auth/sign-up"><p>Sign Up</p></NavLink>
         <NavLink to="/auth/sign-in"><p>Sign In</p></NavLink>
       </div>
+
       <label>Email</label>
-      <input className='email-input'></input>
+
+      <input className='email-input' value={email} onChange={(e) => {
+        setEmail(e.target.value);
+      }}></input>
+
       <label>Password</label>
-      <input className='password-input'></input>
-      <button>Submit</button>
+
+      <input className='password-input' value={password} type="password" onChange={(e) => {
+        setPassword(e.target.value);
+      }}></input>
+
+      <button onClick={() => {
+        authUser(email, password, type);
+      }}>Submit</button>
+
     </div>
 
 
-  </div>;
+  </div>
+  );
 }
 
 export default Auth;
