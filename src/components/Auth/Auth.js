a49d0a8646ea26af50190471341bc1e564c24e4a
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-10-05 21:05:56 -0700

    style auth page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -4,6 +4,12 @@ import { authUser } from '../../services/auth';
 import './Auth.css';
 import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
 
 function Auth() {
   const { type } = useParams();
@@ -24,40 +30,62 @@ function Auth() {
   return (
     <div className="main-auth">
       <div className="container-input">
-        <div className="links-container">
-          <NavLink to="/auth/sign-up">
-            <p>Sign Up</p>
-          </NavLink>
-          <NavLink to="/auth/sign-in">
-            <p>Sign In</p>
-          </NavLink>
-        </div>
-        <form className="input-form">
-          <label>Email</label>
-
-          <input
-            className="email-input"
-            autoComplete="on"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          ></input>
-
-          <label>Password</label>
-
-          <input
-            className="password-input"
-            autoComplete="on"
-            value={password}
-            type="password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          ></input>
-
-          <button onClick={submitAuth}>Submit</button>
-        </form>
+        <Box
+          className="box"
+          sx={{
+            bgcolor: 'background.paper',
+            boxShadow: 1,
+            borderRadius: 2,
+            pt: 0,
+            minWidth: 300,
+            minHeight: 400,
+          }}
+        >
+          <div className="links-container">
+            <NavLink className="nav" to="/auth/sign-up" underline="never">
+              <Button variant="contained" color="secondary" margin="dense">
+                Sign Up
+              </Button>
+            </NavLink>
+            <NavLink className="nav" to="/auth/sign-in" underline="never">
+              <Button variant="contained" color="secondary" margin="dense">
+                Sign In
+              </Button>
+            </NavLink>
+          </div>
+          <FormControl className="input-form">
+            <label>Email</label>
+            <div className="input-icons">
+              <PersonIcon className="icon"></PersonIcon>
+              <TextField
+                className="email-input"
+                autoComplete="on"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              ></TextField>
+            </div>
+            <label>Password</label>
+            <div className="input-icons">
+              <LockIcon className="icon"></LockIcon>
+              <TextField
+                className="password-input"
+                autoComplete="on"
+                value={password}
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              ></TextField>
+            </div>
+            <Box className="button-box">
+              <Button mt={2} variant="contained" className="auth-btn" onClick={submitAuth}>
+                Submit
+              </Button>
+            </Box>
+          </FormControl>
+        </Box>
       </div>
     </div>
   );
