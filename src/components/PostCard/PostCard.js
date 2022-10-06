cce2e708a04745da94c1ef7455425cb454f992f1
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-10-06 12:42:17 -0700

    fix errors

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -17,7 +17,11 @@ export default function PostCard({ title, description, user_id, id }) {
     <div className="post-card">
       <h3 className="title">{title}</h3>
       <p className="description">{description}</p>
-      {owner && <Link to={`/edit-post/${id}`}><button onClick={getPostDetail(id)}>edit</button></Link>}
+      {owner && (
+        <Link to={`/edit-post/${id}`}>
+          <button onClick={() => getPostDetail(id)}>edit</button>
+        </Link>
+      )}
     </div>
   );
 }
